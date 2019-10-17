e2c517f962b78ad15f88353d94178e963a811967
viet <lhviet88@gmail.com>
2019-10-17 17:08:45 +0900

    Update CORS to accept all (for testing)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # To allow any hostname
+  config.hosts.clear
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
